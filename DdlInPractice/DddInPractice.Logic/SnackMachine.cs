0d0d7dcaabf0033ec9347a2959e9479d80bf2c63
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-16 21:52:12 +0100

    Implementing the Snack Machine Class

--- DdlInPractice/DddInPractice.Logic/SnackMachine.cs
+++ DdlInPractice/DddInPractice.Logic/SnackMachine.cs
@@ -1,18 +1,30 @@
-﻿namespace DddInPractice.Logic
+﻿using System;
+using System.Linq;
+using static DddInPractice.Logic.Money;
+
+namespace DddInPractice.Logic
 {
     public class SnackMachine : Entity
-    { 
-        public Money MoneyInside { get; private set; }
-        public Money MoneyInTransaction { get; private set; }
+    {
+        public Money MoneyInside { get; private set; } = None;
+        public Money MoneyInTransaction { get; private set; } = None;
+
 
         public void InsertMoney(Money money)
         {
+            Money[] coinsAndNotes =
+            {
+                Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar
+            };
+            if (!coinsAndNotes.Contains(money))
+                throw new InvalidOperationException();
+
             MoneyInTransaction += money;
         }
 
         public void ReturnMoney()
         {
-            //MoneyInTransaction = 0;
+            MoneyInTransaction = None;
         }
 
         public void BuySnack()
