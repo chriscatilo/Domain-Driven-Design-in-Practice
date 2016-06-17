013f8874129a83230eb226e513771695ddfcbb63
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-17 18:23:54 +0100

    Adding UI for the Snack Machine

--- DdlInPractice/DddInPractice.Tests/MoneySpecs.cs
+++ DdlInPractice/DddInPractice.Tests/MoneySpecs.cs
@@ -1,9 +1,6 @@
-﻿using System;
-
-using DddInPractice.Logic;
-
+﻿using DddInPractice.Logic;
 using FluentAssertions;
-
+using System;
 using Xunit;
 
 namespace DddInPractice.Tests
@@ -130,6 +127,33 @@ namespace DddInPractice.Tests
             };
 
             action.ShouldThrow<InvalidOperationException>();
+        }
+
+
+
+        [Theory]
+        [InlineData(1, 0, 0, 0, 0, 0, "¢1")]
+        [InlineData(0, 0, 0, 1, 0, 0, "$1.00")]
+        [InlineData(1, 0, 0, 1, 0, 0, "$1.01")]
+        [InlineData(0, 0, 2, 1, 0, 0, "$1.50")]
+        public void To_string_returns_correct_string_representation(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount,
+            string expectedString)
+        {
+            Money money = new Money(
+                oneCentCount,
+                tenCentCount,
+                quarterCount,
+                oneDollarCount,
+                fiveDollarCount,
+                twentyDollarCount);
+
+            money.ToString().Should().Be(expectedString);
         }
     }
 }
