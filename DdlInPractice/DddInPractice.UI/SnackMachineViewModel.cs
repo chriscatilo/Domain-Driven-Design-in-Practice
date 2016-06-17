22c7396f3a2659cad9af42016c0ae6a42a7f3ca1
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-17 17:42:34 +0100

    Created UI

---
+++ DdlInPractice/DddInPractice.UI/SnackMachineViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DddInPractice.Logic;
+using DddInPractice.UI.Common;
+
+namespace DddInPractice.UI
+{
+    public class SnackMachineViewModel : ViewModel
+    {
+        private readonly SnackMachine _snackMachine;
+
+        public override string Caption => "Snack Machine";
+
+        public SnackMachineViewModel(SnackMachine snackMachine)
+        {
+            _snackMachine = snackMachine;
+        }
+    }
+}
