6c71464195f3169c0a9c5bfce1b74d04bb3788ea
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-18 17:12:46 +0100

    Introduced Management bounded context

---
+++ DdlInPractice/DddInPractice.Logic/Management/HeadOffice.cs
@@ -0,0 +1,11 @@
+﻿using DddInPractice.Logic.Common;
+using DddInPractice.Logic.SharedKernel;
+
+namespace DddInPractice.Logic.Management
+{
+    public class HeadOffice : AggregateRoot
+    {
+        public virtual decimal Balance { get; protected set; }
+        public virtual Money Cash { get; protected set; } = Money.None;
+    }
+}
